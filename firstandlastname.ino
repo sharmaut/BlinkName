7629e1248a7ea96a21d3446ad9099160065e23a8
sharmaut <40994950+sharmaut@users.noreply.github.com>
2020-03-20 18:01:21 +1100

    Add files via upload

---
+++ firstandlastname.ino
@@ -0,0 +1,67 @@
+int led2 = D7;
+
+void setup() {
+    pinMode(led2, OUTPUT);
+}
+
+void loop() {
+    // The code below is "U T S A V"
+
+    digitalWrite(led2, HIGH);
+    delay(300);
+    digitalWrite(led2, LOW);
+    delay(300);
+    digitalWrite(led2, HIGH);
+    delay(300);
+    digitalWrite(led2, LOW);
+    delay(300);
+    digitalWrite(led2, HIGH);
+    delay(500);
+    digitalWrite(led2, LOW);
+    delay(700);
+    
+    digitalWrite(led2, HIGH);
+    delay(500);
+    digitalWrite(led2, LOW);
+    delay(700);
+    
+    digitalWrite(led2, HIGH);
+    delay(300);
+    digitalWrite(led2, LOW);
+    delay(300);
+    digitalWrite(led2, HIGH);
+    delay(300);
+    digitalWrite(led2, LOW);
+    delay(300);
+    digitalWrite(led2, HIGH);
+    delay(300);
+    digitalWrite(led2, LOW);
+    delay(500);
+    
+    digitalWrite(led2, HIGH);
+    delay(300);
+    digitalWrite(led2, LOW);
+    delay(300);
+    digitalWrite(led2, HIGH);
+    delay(500);
+    digitalWrite(led2, LOW);
+    delay(700);
+    
+    digitalWrite(led2, HIGH);
+    delay(300);
+    digitalWrite(led2, LOW);
+    delay(300);
+    digitalWrite(led2, HIGH);
+    delay(300);
+    digitalWrite(led2, LOW);
+    delay(300);
+    digitalWrite(led2, HIGH);
+    delay(300);
+    digitalWrite(led2, LOW);
+    delay(300);
+    digitalWrite(led2, HIGH);
+    delay(500);
+    digitalWrite(led2, LOW);
+    delay(700);
+    
+}
